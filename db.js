000c6954e756e3f3bf1d25d58043cb6b63809825
Oriol Burgaya <oriol.burgaya@est.fib.upc.edu>
2016-02-02 20:23:52 +0100

    1

--- db.js
+++ db.js
@@ -8,6 +8,6 @@ module.exports = mongoose
 */
 
 var mongoose = require('mongoose')
-var url = process.env.MONGOLAB_URI || 'mongodb://localhost/social'
+var url = 'mongodb://heroku_8jfn3cff:2b3umeaunll5vehlesai6sblh0@ds055515.mongolab.com:55515/heroku_8jfn3cff' || 'mongodb://localhost/social'
 mongoose.connect(url)
 module.exports = mongoose
