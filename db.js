7073bd5872331ae40a9810f4f62842866fff864e
Oriol Burgaya <oriol.burgaya@est.fib.upc.edu>
2016-02-02 20:08:22 +0100

    db change to work on heroku

--- db.js
+++ db.js
@@ -1,5 +1,13 @@
+/* 
+// In localhost
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/social', function() {
 	console.log('mongodb connected')
 })
+module.exports = mongoose
+*/
+
+var mongoose = require('mongoose')
+var url = process.env.MONGOLAB_URI || 'mongodb://localhost/social'
+mongoose.connect(url)
 module.exports = mongoose
