34543d027af0552440e5d373393ef38bd1cb2150
Oriol Burgaya <oriol.burgaya@est.fib.upc.edu>
2016-02-04 00:33:56 +0100

    nav-bar & login template

--- controllers/static.js
+++ controllers/static.js
@@ -5,6 +5,7 @@ var router = express.Router()
 // __dirname is a special Node variable that points to the current file's dirctory (/controllers in this case)
 // You could use process.cwd() instead of __dirname
 router.use(express.static(__dirname + '/../assets'))
+router.use(express.static(__dirname + '/../templates'))
 
 router.get('/', function(req, res) {
 	res.sendfile('layouts/app.html')
