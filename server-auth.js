411165a871dea4c542281803d4bfa25373f2290e
Oriol Burgaya <oriol.burgaya@est.fib.upc.edu>
2016-02-03 20:27:42 +0100

    Auth demo

--- server-auth.js
+++ server-auth.js
@@ -3,6 +3,7 @@ var jwt = require('jwt-simple')
 var app = express()
 var _ = require('lodash')
 var bcrypt = require('bcrypt')
+var User = require('./models/user')
 app.use(require('body-parser').json())
 
 var users = [{username: 'dickeyxxx', password: '$2a$10$NRyZJOvprhz8VQO3F/GRdOzKZkwT.pFJEaSUSGUDU1kLzMBd.ycAy'}]
@@ -17,20 +18,39 @@ function validateUser(user, password, cb) {
     return bcrypt.compare(password, user.password, cb)
 }
 
-app.post('/session', function (req, res) {
-    var user = findUserByUsername(req.body.username)
-    validateUser(user, req.body.password, function (err, valid) {
-        if (err || !valid) {return res.send(401)}
-        var token = jwt.encode({username: user.username}, secretKey)
-        res.json(token)
-    })
+app.post('/session', function (req, res, next) {
+    User.findOne({username: req.body.username})
+        .select('password')
+        .exec(function (err, user) {
+            if (err) { return next(err) }
+            if (!user) { return res.send(401) }
+            bcrypt.compare(req.body.password, user.password, function (err, valid) {
+                if (err) { return next(err) }
+                if (!valid) { return res.send(401) }
+                var token = jwt.encode({username: user.username}, secretKey)
+                res.json(token)
+            })
+    })
 })
 
 app.get('/user', function (req, res) {
-    var token = req.headers['x-auth']
-    var user = jwt.decode(token, secretKey)
-    // TODO: pull user info from database
-    res.json(user)
+    var token = req.headers['x-auth']
+    var auth = jwt.decode(token, secretKey)
+    User.findOne({username: auth.username}, function (err, user) {
+        res.json(user)
+    })
 })
+
+app.post('/user', function (req, res, next) {
+    var user = new User({username: req.body.username})
+    bcrypt.hash(req.body.password, 10, function (err, hash) {
+        user.password = hash
+        user.save(function (err) {
+        if (err) { throw next(err) }
+            res.send(201)
+        })
+    })
+})
+
 
 app.listen(3000)
