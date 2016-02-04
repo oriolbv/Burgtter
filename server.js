33b356469791bb164412c230c2e4e9416830062f
Oriol Burgaya <oriol.burgaya@est.fib.upc.edu>
2016-02-04 01:23:29 +0100

    listen port

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use('/api/sessions', require('./controllers/api/sessions'))
 app.use('/api/users', require('./controllers/api/users'))
 app.use(require('./controllers/static'))	// equivalent to: app.use('/', require('./controllers/static'))
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Server listening on', 3000)
 })
